e815f1db043aad6a94b46542f813811084230fb9
Pranjal Ahuja <53055473+P-ranjal@users.noreply.github.com>
2021-07-01 02:10:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from werkzeug.wrappers import Request, Response
 from  flask import Flask, render_template, request
 import pickle
 import numpy as np
-from  bs4 import BeautifulSoup
+#from  bs4 import BeautifulSoup
 import requests
 import time
 app = Flask(__name__)
@@ -17,10 +17,10 @@ def man():
     return render_template("home.html")
 @app.route('/predict', methods=['POST'])
 def home():
-    url='https://www.google.com/search?q=bitcoin+price'
-    HTML=requests.get(url)
-    soup=BeautifulSoup(HTML.text,'html.parser')
-    text=soup.find('div',attrs={'class':'BNeawe iBp4i AP7Wnd'}).find('div',attrs={'class':'BNeawe iBp4i AP7Wnd'}).text
+    #url='https://www.google.com/search?q=bitcoin+price'
+    #HTML=requests.get(url)
+   # soup=BeautifulSoup(HTML.text,'html.parser')
+    #text=soup.find('div',attrs={'class':'BNeawe iBp4i AP7Wnd'}).find('div',attrs={'class':'BNeawe iBp4i AP7Wnd'}).text
     if request.method=='POST':
         model = pickle.load(open('regt.pkl', 'rb'))
         data1 = request.form['a']
@@ -34,8 +34,8 @@ def home():
         arr = [[data1, data2, data3, data4]]
         pred = model.predict(arr)
         a="The Predicted One"
-        b="The Current Value"
-        l=[pred[0],a,text,b]
+       # b="The Current Value"
+        l=[pred[0],a]
     return render_template('after.html',l=l)
 if __name__ == "__main__":
     from werkzeug.serving import run_simple
